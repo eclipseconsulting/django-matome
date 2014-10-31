d425235cfa6099f59a1afefbbbc7c38dcab65b1c
keitaoouchi <keita.oouchi@gmail.com>
2014-10-31 15:31:45 +0900

    categorized

--- matome/management/commands/matome.py
+++ matome/management/commands/matome.py
@@ -1,8 +1,9 @@
 from django.core.management.base import BaseCommand
 
 import os
+from collections import OrderedDict
 
-from ._code_stats import CodeStats
+from ._code_stats import SummaryPresenter
 
 
 class Command(BaseCommand):
@@ -11,17 +12,44 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        targets = [
-            ('ALL', self._listup())
-        ]
+        targets = self.sniff()
+        output = SummaryPresenter.summarize(targets)
+        self.stdout.write(output)
 
-        self.stdout.write(CodeStats(targets).result)
-
-    def _listup(self):
-        targets = []
+    def sniff(self):
+        targets = OrderedDict()
+        targets['View'] = []
+        targets['Model'] = []
+        targets['Route'] = []
+        targets['Other Modules'] = []
+        targets['JS'] = []
+        targets['Coffee'] = []
         for dname, subdirs, fnames in os.walk('.'):
             for fname in fnames:
                 path = os.path.join(dname, fname)
                 if os.path.isfile(path):
-                    targets.append(path)
+                    category = self.categorize(path)
+                    if category:
+                        if not category in targets:
+                            targets[category] = []
+                        targets[category].append(path)
         return targets
+
+    def categorize(self, fname):
+        ignored_directory = 'node_modules'
+        if ignored_directory in fname:
+            return ''
+        if fname.endswith('views.py'):
+            return 'View'
+        elif fname.endswith('models.py'):
+            return 'Model'
+        elif fname.endswith('urls.py'):
+            return 'Route'
+        elif fname.endswith('.py'):
+            return 'Other Modules'
+        elif fname.endswith('.js'):
+            return 'JS'
+        elif fname.endswith('.coffee'):
+            return 'Coffee'
+        else:
+            return ''
